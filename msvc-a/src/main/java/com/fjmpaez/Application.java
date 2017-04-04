b3ca8e0e314ff9d9bdefb7c3052c27c76510733e
Francisco José Martínez Páez <fj.martinez.paez@gmail.com>
2017-04-04 09:54:49 +0200

    Update Application.java

--- msvc-a/src/main/java/com/fjmpaez/Application.java
+++ msvc-a/src/main/java/com/fjmpaez/Application.java
@@ -3,9 +3,13 @@ package com.fjmpaez;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClients;
+
+import com.fjmpaez.configuration.SPXRibbonConfigurator;
 
 @SpringBootApplication
 @EnableEurekaClient
+@RibbonClients(defaultConfiguration = SPXRibbonConfigurator.class)
 public class Application {
 
     public static void main(String[] args) {
